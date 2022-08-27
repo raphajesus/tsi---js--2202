5c4a072acef115969808262c73c6eb072a2644c9
RAFAEL OLIVEIRA DE JESUS <rafael.ojesus2@senacsp.edu.br>
2022-08-26 22:18:56 -0300

    COndicional, operadores, ternario

---
+++ aquecimento.js
@@ -0,0 +1,82 @@
+// for
+//for (let i = 1; i <=10; i++) {
+//    document.write("linha " +i + "<br>");
+//}
+//let i = 0
+//while (i <= 10) {
+    //console.log("O numero é" + i);
+    //i++;
+  //}
+
+  //let i = 0
+  //do {
+    //console.log("O numero é " + i);
+    //i++;
+  //}
+  //while (i <= 10);
+
+
+//let a =10;
+//let b = 20;
+
+//if (a >b){
+    //console.log(a + 'é maior que' + b);
+//}
+
+    
+//else{
+    
+    // console.log(b + 'é menor '+ b);
+
+//}
+
+//A switchinstrução é usada para executar diferentes ações com base em diferentes condições.
+
+/*
+var dia = 8
+switch (dia) {
+    case 0:
+      console.log ("Segunda-feira");
+      break;
+    case 1:
+        console.log ("Terça-feira");
+      break;
+    case 2:
+        console.log ("Quarta-feira");
+      break;
+    case 3:
+        console.log ("Quinta-feira");
+      break;
+    case 4:
+        console.log ("Sexta-feira");
+      break;
+    case 5:
+        console.log ("Sabado");
+      break;
+    case 6:
+        console.log ("Domingo");
+        break;
+    default:
+        console.log("falso")
+  }
+  //Ternário
+
+  let x = 4;
+  let y = 2;
+  let resultado = x > y ? 'x é maior' : 'não é maior';
+
+  console.log(resultado);
+*/ 
+
+  //exercicio
+  //let genero = prompt(' Entre com M ou F');
+  //let saida = genero == "M" ? 'Masculino': "Feminino";
+
+ // console.log(saida);
+
+  //Coalescente
+  let coisa = 10;
+  let variavel = coisa ?? 'Não ha';
+  console.log(variavel);
+  
+
