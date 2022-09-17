cf07ab61ebc1d48c524077f9a7ee445c21b3238e
RAFAEL OLIVEIRA DE JESUS <rafael.ojesus2@senacsp.edu.br>
2022-09-16 22:10:45 -0300

    Dom continuação

---
+++ dom/ex03.js
@@ -0,0 +1,20 @@
+let e
+
+// e = documemnt.head;
+// e = documemnt.body;
+ //e = documemnt.body.firstElementChild.firstElementChild;
+ //e = document.doctype;
+ //e = document.doctype.name;
+   //e = document.forms;
+    //e = document.forms[0];
+    //e = document.forms[0][1];
+    //e = document.scripts;
+    //e = document.scripts[2];
+    // e = document.scripts[2].getAttribute('src');
+       //e = document.links
+       //e = document.title
+       e = document.querySelector('input#task');
+    
+
+
+console.log(e);
--- dom/ex2.js
+++ dom/ex2.js
@@ -2,14 +2,42 @@ let btnCalcular = document.getElementById('calcular');
 
 btnCalcular.addEventListener('click', function (evento){
     evento.preventDefault();
+
+    let serv = document.getElementById('servico');
+    let prazo = document.getElementById('prazo');
+    let res =  document.getElementById('resultado');
+
+    /*recuperar os dados da matriz*/
+    let preco = calculaPreco(serv.value,prazo.value);
+
+ // mostrar o resultado no elemento p
+    res.innerText =`O preço é ${preco}`;
+
+   
+//ver dados do evento (click) no console
+  // console.log(tabela[serv][prazo]);
+    
+
+    
+    //ver dados do evento
     console.log (evento);
 
     //obter os valores de  serviço e prazo
 });
 
+function calculaPreco(servico,prazo){
+    
+    let tabela = [ ['$$$','$$','$'],
+    ['$$$$',"$$$","$$"],
+    ['--','--','$$$']];
+    console.log(tabela[servico][prazo]);
+    return tabela[servico][prazo];
+
+}
+
    
 
-btnCalcular.addEventListener('click' , executa ());
+/*btnCalcular.addEventListener('click' , executa ());
 
 function executa ({
     alert('Clicou');
@@ -17,4 +45,6 @@ function executa ({
 */
 
 //console.log(btnCalcular);
+
+
 
