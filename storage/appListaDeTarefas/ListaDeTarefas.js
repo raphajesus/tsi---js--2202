a3ff696a21fa7dcfb75aa74c20790f0bfdcff883
RAFAEL OLIVEIRA DE JESUS <rafael.ojesus2@senacsp.edu.br>
2022-10-21 22:07:06 -0300

    21-10-2022

--- storage/appListaDeTarefas/ListaDeTarefas.js
+++ storage/appListaDeTarefas/ListaDeTarefas.js
@@ -16,12 +16,59 @@ function carregaMonitoresDeEventos(){
     btnAddTarefa.addEventListener('click', adicioneTarefa);
      // evento para fazer o x funcionar (apagar uma unica tarefa)
     listaDeTarefas.addEventListener('click', apagarTarefa);
+    // evento para limpar toda lista
+    btnLimpaTudo.addEventListener('click',apagarTudo);
+    //evento para filtrar as tarefas
+    filtroDeTarefa.addEventListener('keyup', filtrar);
 
 }
 
 carregaMonitoresDeEventos();
 
+function filtrar(evento){
+
+    //capturar o que o usuario esta digitando
+    //passando tudo para minisculo
+    const procurado = evento.target.value.toLowerCase();
+    //capturamos todos elementos li existentes
+     const tarefas = console.log(document.querySelectorAll('collection-item'));
+
+     //Para cada tarefa existente,busque a string
+     // desejada
+     tarefas.forEach(function(tarefa){
+
+
+        //Recuperamos apenas o texto do elemento
+        // li onde esta a tarefa
+         textoTarefa = tarefa.innerText;
+
+         //Procuramos a string digitada no filtro
+         // no texto que esta no <li>
+         if(textoTarefa.toLowerCase().indexof(procurado)!= -1){
+
+            tarefa.style.display = 'block';
+
+         }else{
+            tarefa.style.display = 'none';
+
+         }
+
+     });
+
+
+}
+
+
+function apagarTudo(evento){
+    evento.preventDefault();
+
+    listaDeTarefas.innerHTML='';
+
+}
+
+
 function apagarTarefa(evento){
+          
     // se o elemento pai tiver a classe apaga-tarefa,
     // ou seja, for o elemento "a",apague li,ou seja
     //a tarefa  
@@ -44,6 +91,7 @@ function adicioneTarefa(evento){
     if(entradaTarefa.value === ''){
 
         alert('Entre com uma tarefa');
+        return false;
 
     }
 
@@ -88,7 +136,23 @@ function adicioneTarefa(evento){
     //apaga o input para entrada
 
     entradaTarefa.value = '';
+     
+    //gravar a tarefa no localStorage
+    gravarTarefa(entradaTarefa.value);
+}
 
+function gravarTarefa(tarefa){
 
+    let tarefas = [];
 
+    let tarefaDoStorage = localStorage.getItem('tarefas');
+
+    if(tarefaDoStorage != null){
+
+        tarefas = JSON.parse(tarefaDoStorage);
+
+    }
+       tarefas.push(tarefa);
+
+       localStorage.setItem('tarefas',JSON.stringify(tarefas));
 }
